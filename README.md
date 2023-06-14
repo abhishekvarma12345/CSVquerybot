7ded272d0e0d8c118df227f3cf784b33673f7ef0
abhishekvarma12345 <abhishekvarmad@gmail.com>
2023-06-14 17:27:18 +0200

    first working version

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CSVquerybot
 This Generative LLM bot helps to extract useful insights from the given csv file in a simple QA fashion.
+
+**Functionalities:**
+1. Ask user to upload a CSV file.
+    - using chainlit for conversation between user and bot.
+    - read the CSV file into pandas DataFrame object.
+2. Process the message of the user to answer the queries on the uploaded CSV file.
+    - Use langchain to create CSV agent to answer user questions on the CSV file.
+
+
--- app.py
+++ app.py
@@ -1,11 +1,45 @@
+import os
+import pandas as pd
+from langchain.agents import create_pandas_dataframe_agent
+from langchain.llms import OpenAI
 import chainlit as cl
+from Lib.io import BytesIO
 
+CUSTOM_PREFIX = """
+You are working with a pandas dataframe in Python. The name of the dataframe is `df`.
+if Question is about the amount then report in euros. 
+You should use the tools below to answer the question posed of you:"""
+llm = OpenAI(temperature=0)
+
+
+@cl.on_chat_start
+async def ask_file():
+    files = None
+
+    while files == None:
+        files = await cl.AskFileMessage(
+            content = "Upload a CSV file to begin analysis!",
+            accept=["text/csv"],
+            max_size_mb=100,
+            timeout=120
+        ).send()
+    
+    file = files[0]
+    cl.user_session.set("data", file.content) # storing the content to user session
+    
+    msg = cl.Message(content=f"{file.name} Upload Successful!")
+    await msg.send()
+    await cl.sleep(2)
+    await msg.update(content="I am ready to answer the queries on {file.name}.")
+    
 
 @cl.on_message
-def main(message: str):
-    # Your custom logic goes here...
+async def request(question: str):
+    bytes_data = cl.user_session.get("data")
+    df = pd.read_csv(BytesIO(bytes_data))
+    agent = create_pandas_dataframe_agent(llm, df, prefix=CUSTOM_PREFIX, verbose=True)
+    result = agent.run(question)
+    await cl.Message(content=f"{result}").send()
 
-    # Send a response back to the user
-    cl.Message(
-        content=f"Received: {message}",
-    ).send()
+
+
